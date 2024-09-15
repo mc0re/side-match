9ddef53db622644f3c8690f65c1a8b038f573434
Mike <mnikitin0@gmail.com>
2024-09-15 21:37:58 +0200

    Adding tests

--- tests/test_Piece.py
+++ tests/test_Piece.py
@@ -2,9 +2,16 @@ import pytest
 from src.Piece import Piece
 
 
-def test_rotate():
-    p = Piece(1, [2, 3, 4])
+def test_init():
+    p = Piece(1, [2, 3, 4, 5])
     assert p.number == 1
+    assert p.patterns == [2, 3, 4, 5]
+
+
+def test_rotate():
+    p = Piece(1, [2, 3, 4, 5])
+    p.rotate_clockwise()
+    assert p.patterns == [5, 2, 3, 4]
 
 
 if __name__ == "__main__":
